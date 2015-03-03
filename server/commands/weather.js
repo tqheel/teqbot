e2dc331c9e299200297dbada43d49bded33fb58e
Todd Qualls <tqualls@gmail.com>
2015-03-02 23:25:53 -0500

    Fleshes out weather retrieval and conversion to F, along with a couple of other basic and stoopid commands.

---
+++ server/commands/weather.js
@@ -0,0 +1,38 @@
+var http = require("http");
+var openWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+
+
+function convertKelvinToCelsius(kelvin){
+	return kelvin - 273.15;
+}
+
+function convertKelvinToFahrenheit(kelvin){
+	var celsius = convertKelvinToCelsius(kelvin);
+	return Math.round((celsius * 1.8) + 32);
+}
+
+function getWeatherByCity(city, callback){
+	return http.get(openWeatherUrl + encodeURIComponent(city), function(response) {
+		response.setEncoding('utf8');
+		var responseData = '';
+		response.on('data', function(chunk){
+			responseData += chunk;
+		});
+		response.on('error', function(error){
+			error.forEach(function(e){
+				console.log(e);
+			});
+		});
+		response.on('end', function(){
+			var jsonData = JSON.parse(responseData);
+			var temp =  convertKelvinToFahrenheit(jsonData.main.temp);
+			var condition = jsonData.weather[0].main;
+			return callback('In '+ city + ', the current temperature is ' + temp + ' and '+ condition);
+		});	
+	});
+}
+
+module.exports = {
+	getWeatherByCity: getWeatherByCity
+};
+
---
+++ server/server.js
@@ -0,0 +1,66 @@
+var net = require('net');
+var weather = require('./commands/weather.js');
+
+var botname = 'teqbot';
+
+var port = 1337;
+var host = '127.0.0.1';
+
+var commands = [
+		'weather',
+		'hello',
+		'goodbye'
+];
+
+function createTimeString(){
+	date = new Date();
+	var y = date.getFullYear();
+	var m = zeroPadIfNeeded(date.getMonth()+1);
+	var d = zeroPadIfNeeded(date.getDate());
+	var h = zeroPadIfNeeded(date.getHours());
+	var min = zeroPadIfNeeded(date.getMinutes());
+	var dash = '-';
+	return y+dash+m+dash+d+' '+h+':'+min;
+}
+function zeroPadIfNeeded(input){
+	return (input<10) ? '0' + input.toString() : input.toString();
+}
+function processMessage(msg, callback){
+	var response = '';
+	switch(msg.toString().toLowerCase()){
+		case commands[0]:
+			weather.getWeatherByCity('Cary, NC, USA', callback);
+			break;
+		case commands[1]:
+			callback('Hi. How are you?');
+			break;
+		case commands[2]:
+			callback('Leaving so soon? That makes ' + botname + ' so sad. :-(');
+			break;
+		default: 
+			callback('You said nothing of consequence. Available commands are as follows: ' + commands);
+	}
+}
+
+
+
+
+var server = net.createServer(function(socket){
+	socket.on('error', function(error){
+		console.log('Empty request received.');
+
+	});
+	socket.on('data', function(data){
+		console.log('Received command: '+data + ' from '+socket.remoteAddress);
+		var time = createTimeString();
+		processMessage(data, function(response){
+			socket.write(time + '\n' + botname + ': ' + response);
+		});
+		
+	});
+	
+});
+server.listen(port,host);
+
+console.log('Server listening on '+ host+':'+port);
+
